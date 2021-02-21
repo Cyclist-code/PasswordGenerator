ea95a408c680a181690413ec28a42b2ed8e5351c
Gostev Konstantin <47049219+Cyclist-code@users.noreply.github.com>
2021-02-21 14:18:28 +0700

    Writing logic to generate a password

--- PasswordGenerator/MainWindow.xaml.cs
+++ PasswordGenerator/MainWindow.xaml.cs
@@ -36,5 +36,60 @@ namespace PasswordGenerator
         {
             DragMove();
         }
+
+        private void GeneratePasswordBtn_Click(object sender, RoutedEventArgs e)
+        {
+            GeneratePassword();
+        }
+
+        private void GeneratePassword()
+        {
+            string generatedPassword = "";
+            string[] arrayCharacters =
+            {
+                "0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G","H","I","J",
+                "K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d",
+                "e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"
+            };
+
+            Random randomCharacters = new Random();
+
+            if (EightCharactersRB.IsChecked == true)
+            {
+                for(int i = 0; i < 8; i++)
+                {
+                    generatedPassword += arrayCharacters[randomCharacters.Next(0, 61)];
+                    OutputGeneratedPassword.Text = generatedPassword;
+                }
+            }
+
+            if (TenCharactersRB.IsChecked == true)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    generatedPassword += arrayCharacters[randomCharacters.Next(0, 61)];
+                    OutputGeneratedPassword.Text = generatedPassword;
+                }
+            }
+
+            if (TwelveCharactersRB.IsChecked == true)
+            {
+                for (int i = 0; i < 12; i++)
+                {
+                    generatedPassword += arrayCharacters[randomCharacters.Next(0, 61)];
+                    OutputGeneratedPassword.Text = generatedPassword;
+                }
+            }
+
+            if (SixteenCharactersRB.IsChecked == true)
+            {
+                for (int i = 0; i < 16; i++)
+                {
+                    generatedPassword += arrayCharacters[randomCharacters.Next(0, 61)];
+                    OutputGeneratedPassword.Text = generatedPassword;
+                }
+            }
+        }
+        
     }
 }
