4ef515c9397a666cc9e4df1726a4db8d556610a2
Gostev Konstantin <47049219+Cyclist-code@users.noreply.github.com>
2021-02-27 17:33:17 +0700

    Writing the first logic in the view model class

--- PasswordGenerator/ViewModels/GeneratorPasswordViewModel.cs
+++ PasswordGenerator/ViewModels/GeneratorPasswordViewModel.cs
@@ -1,13 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using PasswordGenerator.Commands;
 
 namespace PasswordGenerator.ViewModels
 {
-    public class GeneratorPasswordViewModel
+    public class GeneratorPasswordViewModel : INotifyPropertyChanged
     {
+        public GeneratorPasswordViewModel()
+        {
 
+        }
+
+        #region Commands
+
+        #endregion
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
     }
 }
